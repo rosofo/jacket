a72d5819593a3f5683025811c6fd606a14fc450e
Robin Forbes <rsforbes0@gmail.com>
2025-08-02 16:06:13 +0100

    setup a service worker

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@
 /// <reference types="@vitest/browser/providers/playwright" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: false,
+    }),
+  ],
   test: {
     projects: [
       {
