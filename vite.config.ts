213d0fc51d9907e3bc880e8a8f44aa2d01d70d16
Robin Forbes <rsforbes0@gmail.com>
2025-08-05 20:39:00 +0100

    up the workbox limit

--- vite.config.ts
+++ vite.config.ts
@@ -5,65 +5,83 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: "autoUpdate",
-    devOptions: {
-      enabled: true
-    },
-    manifest: false
-  })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: false,
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3072,
+      },
+    }),
+  ],
   test: {
-    projects: [{
-      test: {
-        // an example of file based convention,
-        // you don't have to follow it
-        include: ["**/*.{test,spec}.ts", "**/*.unit.{test,spec}.ts"],
-        name: "unit",
-        environment: "node"
-      }
-    }, {
-      test: {
-        // an example of file based convention,
-        // you don't have to follow it
-        include: ["**/*.{test,spec}.browser.ts"],
-        name: "browser",
-        browser: {
-          enabled: true,
-          instances: [{
-            browser: "chromium",
-            launch: {
-              args: ["--remote-debugging-port=9222"]
-            }
-          }]
-        }
-      }
-    }, {
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
+    projects: [
+      {
+        test: {
+          // an example of file based convention,
+          // you don't have to follow it
+          include: ["**/*.{test,spec}.ts", "**/*.unit.{test,spec}.ts"],
+          name: "unit",
+          environment: "node",
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+      },
+      {
+        test: {
+          // an example of file based convention,
+          // you don't have to follow it
+          include: ["**/*.{test,spec}.browser.ts"],
+          name: "browser",
+          browser: {
+            enabled: true,
+            instances: [
+              {
+                browser: "chromium",
+                launch: {
+                  args: ["--remote-debugging-port=9222"],
+                },
+              },
+            ],
+          },
+        },
+      },
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
 });
